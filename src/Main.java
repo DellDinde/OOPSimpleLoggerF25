5d15133716da027418aed60b13d208e2fc956a1c
Chemical Bonds <delldinde8@gmail.com>
2025-09-30 14:25:06 -0400

    Added the SimpleLogger Class.

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,10 @@
+import java.text.SimpleDateFormat;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-
-
+        SimpleLogger logger = new SimpleLogger();
+        logger.logThis("Hello");
     }
 }
