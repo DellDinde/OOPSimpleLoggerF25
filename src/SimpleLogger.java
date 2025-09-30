5327646c6004f82b0574b90de7684886c9cd8a0f
Chemical Bonds <delldinde8@gmail.com>
2025-09-30 14:46:14 -0400

    Added TimeStamp into log message

--- src/SimpleLogger.java
+++ src/SimpleLogger.java
@@ -1,5 +1,14 @@
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class SimpleLogger {
+
+    private static final String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
     public void logThis(String pMessage){
-        System.out.printf(pMessage);
+        String timeStamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern(DATE_FORMAT));
+        String logEntry = String.format("[%s] %s", timeStamp, pMessage);
+        System.out.printf(logEntry);
     }
 }
